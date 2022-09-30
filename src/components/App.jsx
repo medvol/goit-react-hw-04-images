6ce1acb7cde5d0338f0cf9199edb21d952072e66
Oleh Medvid <olmedvid@gmail.com>
2022-09-30 20:58:50 +0300

    fixed: new query return empty images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,7 +17,8 @@ export const App = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [disableButton, setDisableButton] = useState(false);
 
-  const addQuery = async (query) => {     
+  const addQuery = async (query) => {   
+    setImages([]);
     setQuery(query);
     setPage(1);
     setDisableButton(false);  
@@ -63,70 +64,3 @@ export const App = () => {
     )    
   
 }
-
-// export class App extends Component {
-//   state = {
-//     images: [],
-//     query: '',
-//     page: 1,
-//     error: null,
-//     isLoading: false,
-//     disableButton: false
-//   }
-
-  
-//     async componentDidUpdate(_, prevState) {        
-     
-//       const { query: prevImageName, page: prevPage } = prevState;
-//       const { query: nextImageName, page: nextPage } = this.state;
-    
-//       if (prevImageName !== nextImageName || prevPage !== nextPage) {
-
-//           try {
-//               this.setState({ isLoading: true });
-
-//               const images = await getImages(nextImageName, nextPage);
-//             if (images.total === prevState.images.length) {
-//                 this.setState({disableButton: true}) 
-//                 return toast.info(`No more ${nextImageName} to load`, { theme: "colored" })
-//               }
-//               if (images.hits.length === 0) return toast.info(`We do not find ${nextImageName}`, { theme: "colored" });
-              
-//               this.setState(prevState => (
-//                   {
-//                       images: [...prevState.images, ...images.hits],
-//                       isLoading: false
-//                   }
-//               ));
-      
-//           } catch (error) { this.setState({ error }) }
-//           finally { this.setState({isLoading: false}) }
-//       }
-//     }
-
-//   addQuery = async (query) => {     
-//     this.setState({ query, images: [], page: 1, disableButton: false });  
-//   }
-
-//   incrementPage = () => {
-//     this.setState(prevState => ({
-//       page: prevState.page + 1
-//     }))
-// }
-
-//   render() {
-//     const { images, error, isLoading, disableButton } = this.state;
-//     const { addQuery, incrementPage } = this;
-
-//     return (
-//       <Box display="grid" gridTemplateColumns="1fr" gridGap={4} pb={5}>
-//         <Searchbar onSubmit={addQuery}  />
-//         {images.length ? <ImageGallery images={images} /> : null  }      
-//         {isLoading && <Loader /> }
-//         { error && <h1>{error.message}</h1> }        
-//         {images.length ? <LoadMore type='button' text="Load more" onClick={incrementPage} disable={disableButton} /> : null}
-//         <ToastContainer autoClose={3000} position="top-right" />      
-//       </Box>    
-//     )    
-// }
-// }
